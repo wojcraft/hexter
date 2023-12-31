e5e150665036dfcd46dbf743179c458e0234b099
wojcraft <unknown>
2023-12-31 19:11:33 +0100

    Migrate to Data Attributes, Display hex value of color on page title and color the string that displays hex string

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ const hex = [0,1,2,3,4,5,6,7,8,9,"A","B","C","D","E","F"]
 
 const main = document.querySelector("main")
 const btn = document.querySelector(".flip")
-const color = document.querySelector(".color")
+const color = document.querySelector("span.color")
 const link = document.querySelector("a")
 
 const getRandomNumber = () => {
@@ -19,6 +19,12 @@ const flip = () => {
   color.textContent = hexColor
   document.body.style.background = hexColor
   link.style.color = hexColor
+
+  color.dataset.textColor = hexColor
+  let attrTextColor = color.dataset.textColor
+  color.style.color = attrTextColor
+
+  document.title = `${hexColor} - Hexter`
 }
 
 btn.addEventListener("click", flip)
