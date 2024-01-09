c178338470fe436c7ec04671faf3fe1994a4214c
wojcraft <unknown>
2024-01-09 16:50:15 +0100

    Migrate to better method suggested by an experienced friend

--- src/main.js
+++ src/main.js
@@ -1,21 +1,32 @@
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
-
 const btn = document.querySelector('.flip');
-const color = document.querySelector('span.color');
+const displayColor = document.querySelector('span.color');
 
-const getRandomNumber = () => Math.floor(Math.random() * hex.length);
+const getColor = () => {
+  const range = 256;
+
+  let red = Math.floor(Math.random() * range);
+  let green = Math.floor(Math.random() * range);
+  let blue = Math.floor(Math.random() * range);
+
+  let hexRed = red.toString(16);
+  let hexGreen = green.toString(16);
+  let hexBlue = blue.toString(16);
+
+  if (hexRed.length === 1) hexRed = '0' + hexRed;
+  if (hexGreen.length === 1) hexGreen = '0' + hexGreen;
+  if (hexBlue.length === 1) hexBlue = '0' + hexBlue;
+
+  let color = `#${hexRed}${hexGreen}${hexBlue}`;
+  return color;
+};
 
 const flip = () => {
-  let hexColor = '#';
+  const generatedColor = getColor();
 
-  for (let i = 0; i < 6; i++) {
-    hexColor += hex[getRandomNumber()];
-  }
+  displayColor.textContent = generatedColor;
+  document.body.style.background = generatedColor;
 
-  color.textContent = hexColor;
-  document.body.style.background = hexColor;
-
-  document.title = `${hexColor} - Hexter`;
+  document.title = `${generatedColor} - Hexter`;
 };
 
 btn.addEventListener('click', flip);
